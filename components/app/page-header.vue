8812a282d0c6e2057506a09a3a09c4554045b6e7
xiaoluabei <xiaoluabei@gmail.com>
2024-11-06 16:09:11 +0800

    feat: AppPageHeader

---
+++ components/app/page-header.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <div class="mb-8">
+    <h2 class="text-2xl font-bold mb-6 chakra">
+      {{ title }}
+    </h2>
+    <slot />
+  </div>
+</template>
--- pages/bookmark.vue
+++ pages/bookmark.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>bookmark</div>
+  <div>
+    <AppPageHeader title="Bookmarks">
+      <p>
+        Awesome things I've found on the internet. This page is still WIP, I want to add search like
+        <a
+          href="https://bmrks.com"
+          target="_blank"
+          class="text-teal-500 underline"
+        >bmrks.com</a>
+      </p>
+    </AppPageHeader>
+  </div>
 </template>

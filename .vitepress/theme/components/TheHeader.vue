edee301a2bf36f46589f7388ac94a2ae73a17349
xiaoluabei <xiaoluabei@gmail.com>
2025-01-08 13:33:45 +0800

    logo position

--- .vitepress/theme/components/TheHeader.vue
+++ .vitepress/theme/components/TheHeader.vue
@@ -1,13 +1,13 @@
 <template>
   <header class="border-b-4 border-black px-8 py-4">
-    <div class="relative mx-auto max-w-screen-md flex flex-col items-center justify-between gap-6 md:flex-row">
+    <div class="relative mx-auto max-w-screen-md flex items-center justify-between gap-6">
       <span>
         <a href="/">
           <Logo class="absolute top-[-6px] left-0" />
         </a>
       </span>
 
-      <div class="flex items-center gap-6">
+      <div class="flex items-center gap-3">
         <ButtonPrimary href="/blog">
           Blog
         </ButtonPrimary>

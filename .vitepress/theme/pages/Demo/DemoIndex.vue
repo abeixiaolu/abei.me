a3bde58fe4b4f22f4a29894f1a691334d802d843
xiaoluabei <xiaoluabei@gmail.com>
2025-02-15 09:35:56 +0800

    demo: masonry layout

--- .vitepress/theme/pages/Demo/DemoIndex.vue
+++ .vitepress/theme/pages/Demo/DemoIndex.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
   <section class="space-y-8 md:space-y-16">
     <PageHeader />
 
-    <div class="space-y-6">
+    <div class="columns-1 md:columns-2 gap-6">
       <DemoCard
         title="Sprite Animation"
         name="TwitterLikeBtn/TwitterLikeBtn"
         question="inline-flex 可能会造成元素下方出现空白？"
         answer="inline-flex 元素会按照文本基线对齐，浏览器会为其保留降部字形（如 g、p）的空间，即使没有文字也会显示这个空白区域。使用 vertical-align: middle 可以解决这个问题。"
         link="https://leanrada.com/notes/css-sprite-sheets/"
+        class="mb-4 break-inside-avoid"
       >
         <TwitterLikeBtn />
       </DemoCard>
@@ -20,6 +20,7 @@
       <DemoCard
         title="Checkbox"
         name="Checkbox/Checkbox"
+        class="mb-4 break-inside-avoid"
       >
         <Checkbox />
       </DemoCard>
@@ -27,6 +28,7 @@
       <DemoCard
         title="Blur Image Card"
         name="BlurImageCard/BlurImageCard"
+        class="mb-4 break-inside-avoid"
       >
         <BlurImageCard />
       </DemoCard>

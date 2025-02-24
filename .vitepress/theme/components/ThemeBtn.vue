f207656355a3558b56f620de3ac20cd1f656d884
xiaoluabei <xiaoluabei@gmail.com>
2025-02-24 16:41:18 +0800

    feat: theme transition and scrollbar color

--- .vitepress/theme/components/ThemeBtn.vue
+++ .vitepress/theme/components/ThemeBtn.vue
@@ -3,8 +3,43 @@ import { useData } from 'vitepress'
 
 const { isDark } = useData()
 
-function toggleDark() {
-  isDark.value = !isDark.value
+function enableTransitions() {
+  return 'startViewTransition' in document
+    && window.matchMedia('(prefers-reduced-motion: no-preference)').matches
+}
+
+async function toggleDark() {
+  if (!enableTransitions()) {
+    isDark.value = !isDark.value
+    return
+  }
+
+  /*  const clipPath = [
+    `circle(0px at ${x}px ${y}px)`,
+    `circle(${Math.hypot(
+      Math.max(x, innerWidth - x),
+      Math.max(y, innerHeight - y),
+    )}px at ${x}px ${y}px)`,
+  ] */
+
+  const clipPath = [
+    `inset(0 100% 0 0)`,
+    `inset(0 0 0 0)`,
+  ]
+
+  await document.startViewTransition(async () => {
+    isDark.value = !isDark.value
+    await nextTick()
+  }).ready
+
+  document.documentElement.animate(
+    { clipPath: isDark.value ? clipPath.reverse() : clipPath },
+    {
+      duration: 300,
+      easing: 'ease-in',
+      pseudoElement: `::view-transition-${isDark.value ? 'old' : 'new'}(root)`,
+    },
+  )
 }
 </script>
 
@@ -13,3 +48,21 @@ function toggleDark() {
     <div :class="isDark ? 'i-solar-moon-sleep-linear' : 'i-solar-water-sun-linear'" />
   </button>
 </template>
+
+<style>
+::view-transition-old(root),
+::view-transition-new(root) {
+  animation: none;
+  mix-blend-mode: normal;
+}
+
+::view-transition-old(root),
+.dark::view-transition-new(root) {
+  z-index: 1;
+}
+
+::view-transition-new(root),
+.dark::view-transition-old(root) {
+  z-index: 9999;
+}
+</style>

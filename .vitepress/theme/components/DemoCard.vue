7d5c714ea9eceeddab2206b637473d436468f636
xiaoluabei <xiaoluabei@gmail.com>
2025-03-05 23:14:07 +0800

    update theme

--- .vitepress/theme/components/DemoCard.vue
+++ .vitepress/theme/components/DemoCard.vue
@@ -10,7 +10,7 @@ defineProps<{
 
 <template>
   <BorderContainer size="sm">
-    <div class="flex items-center justify-between border-b-2 border-black px-4 py-2">
+    <div class="flex items-center justify-between border-b-2 border-black dark:border-[#506C59] px-4 py-2">
       <h3
         class="text-xl font-semibold flex gap-1"
       >
@@ -21,14 +21,14 @@ defineProps<{
           {{ title }}
         </span>
       </h3>
-      <Button :href="`https://github.com/abeixiaolu/artsmp.me/tree/main/src/demo/${name}.vue`" target="_blank">
-        view code
-      </Button>
+      <a class="link" :href="`https://github.com/abeixiaolu/artsmp.me/tree/main/src/demo/${name}.vue`" target="_blank">
+        code
+      </a>
     </div>
     <div class="p-4">
       <slot />
     </div>
-    <div v-if="question && answer" class="border-t-2 border-black p-4 space-y-2">
+    <div v-if="question && answer" class="border-t-2 border-black dark:border-[#506C59] p-4 space-y-2">
       <h4 class="text-sm font-bold">
         问题说明
       </h4>
